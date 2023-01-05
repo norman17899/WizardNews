ca90141e52cfc385567edf6e957901a682159ae5
Hunter McKinney <hmckinney92@gmail.com>
2023-01-05 11:06:34 -0600

    posts showing up

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
+const postBank = require('./postBank')
+
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/", (req, res) => {
+
+  const posts = postBank.list();
+
+  const html = `<!DOCTYPE html>
+  <html>
+  <head>
+    <title>Wizard News</title>
+ </head>
+  <body>
+  <h1>Wizard News</h1>
+    <ul>
+  ${posts.map(post => `<li>${post.title}</li>`).join('')}
+    </ul>
+  </body>
+  </html>`;
+
+res.send(html)});
 
 const PORT = 1337;
 
